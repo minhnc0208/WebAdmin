591f6a3aa892aba537c984c3a8ae4c747e42af1f
minhlunscs <minhncps08611@fpt.edu.vn>
2020-10-06 16:27:19 +0700

    finishipcomputer

--- index.js
+++ index.js
@@ -766,6 +766,34 @@ router.post("/deleteHoaDon", (req, res) => {
 
   res.redirect(200, "/hoadon");
 });
+
+
+//// Lấy IP theo máy tính
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+        }
+
+        if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            console.log(ifname + ':' + alias, iface.address);
+        } else {
+            // this interface has only one ipv4 adress
+            console.log(ifname, iface.address);
+        }
+        ++alias;
+    });
+});
+
+
+
 app.use("/", router);
 
 app.use("/css", express.static(__dirname + "/css"));
